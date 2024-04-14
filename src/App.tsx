be31156eb3ca206fefab78798f04bd68f2fe3e94
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2024-04-13 20:03:34 -0700

    [ADD] Empezando a desarrollar la calculadora de propinas, creando el Header

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <>
+      <header className="">
+        <h1>Calculadora de propinas
+          
+        </h1>
+      </header>
+    </>
+  )
+}
+
+export default App
